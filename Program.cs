0cc710cbf35bf94c6b99038de64ec037a9229737
gregnani <fgregnani@gmail.com>
2023-04-02 18:38:57 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Contagem Regressiva");
+
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
